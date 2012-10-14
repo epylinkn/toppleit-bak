7750335ad861843183e09a7eb1e26a7851d34743
ant <anthonybui@gmail.com>
2012-10-14 14:26:52 -0700

    fresh project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toppleit::Application.config.secret_token = '2cca084772705d8bea57846a9ec6e10dc46d4440d21985bee8e6df6a90f4143c39d6a310bdaaace29587cc736d51f5935b60883295cbdfe8a1f94e6a983f7b1f'
