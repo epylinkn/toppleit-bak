90b9d3c39a8ee5ae01cac9506e1f43e64e8e69b4
Anthony Bui <anthonybui@gmail.com>
2012-10-20 19:26:47 -0700

    hide signup routes so users cant be created for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Toppleit::Application.routes.draw do
   
-  get 'signup', to: 'users#new', as: 'signup'
+  # get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   root :to => "posts#index"
   
-  resources :posts, :users, :sessions
+  resources :posts, :sessions
+  
+  resources :users, :except => [:new, :create]
   
   namespace :admin do
     resources :posts
